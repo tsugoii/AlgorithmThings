237172d8c9abe6971bbb4a520c5125e38f0ac395
Tsugoii <109778420+tsugoii@users.noreply.github.com>
2024-11-14 20:57:23 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _451_1
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            double result1 = 1;
+            double result2 = 0;
+            Console.WriteLine("I work");
+            for (double n = 0; result1 > result2; n++)
+            {
+                result1 = (10 * (Math.Pow(n, 2.0) + 6));
+                result2 = ((21 * n) + 7);
+                Console.WriteLine("Algorithm One: " + result1);
+                Console.WriteLine("Algorithm Two: " + result2);
+                Console.ReadKey();
+            }
+            Console.WriteLine("I'm Done");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _451_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double evaluate(double[] coefficients, double x)
+            {
+                double result = coefficients[0]; double power = 1;
+                for (int i = 1; i < coefficients.length; i++)
+                {
+                    power = power * x;
+                    result = result + coefficients[i] * power;
+                }
+                return result;
+            }
+        }
+    }
+}
